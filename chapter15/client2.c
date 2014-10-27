be0e4806998d66ec2d326a68add477ce83d776b5
liang.wang <liang.wang@ck-telecom.com>
2014-10-27 17:05:35 +0800

    test client connect
    [id]:
    [Solution Description]
    modified:   chapter15/client2.c
    [Other info]
    N/A

--- chapter15/client2.c
+++ chapter15/client2.c
@@ -7,15 +7,17 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     int sockfd;
     int len;
+	int i;
     struct sockaddr_in address;
     int result;
-    char ch = 'A';
-
+	unsigned char acde0005[5] = {0xAC, 0xED, 0x00, 0x05, 0};
+	unsigned char readbuf[128] = {0};
 /*  Create a socket for the client.  */
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -24,7 +26,7 @@ int main()
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    address.sin_port = 9734;
+    address.sin_port = 8000;
     len = sizeof(address);
 
 /*  Now connect our socket to the server's socket.  */
@@ -38,9 +40,12 @@ int main()
 
 /*  We can now read/write via sockfd.  */
 
-    write(sockfd, &ch, 1);
-    read(sockfd, &ch, 1);
-    printf("char from server = %c\n", ch);
+//    write(sockfd, &ch, 1);
+    read(sockfd, &readbuf, 4);
+
+	for(i =0;i<4;i++) {
+		printf("char from server = %x\n", readbuf[i]);
+	}
     close(sockfd);
     exit(0);
 }
